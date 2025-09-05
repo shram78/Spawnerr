8c59c97461966ee722621ca1b4359c1d65c6829b
Aleksandr Shram <ra00275@gmail.com>
2025-09-04 20:20:17 -0700

    switched Unity 6000.0.50f1

--- Assets/Scripts/ScoreView.cs
+++ Assets/Scripts/ScoreView.cs
@@ -6,12 +6,12 @@ public class ScoreView : MonoBehaviour
    [SerializeField] private TextMeshProUGUI _PlayerLivesText;
    [SerializeField] private TextMeshProUGUI _EnemyKilledText;
 
-   public void ShowPlayerLives(int lives)
+   public void ShowPlayerLives(int lives) //needs rename
    {
       _PlayerLivesText.text = $"Lives : {lives}";
    }
 
-   public void ShowEnemyKilled(int  killed)
+   public void ShowEnemyKilled(int  killed) // needs rename
    {
       _EnemyKilledText.text = $"Killed : {killed}";
    }
